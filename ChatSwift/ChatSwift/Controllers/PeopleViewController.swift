3e07e426a00f5baadd56933e5c414e9471ba2f52
Pavel Chernyshev <titan100695@yandex.ru>
2021-03-14 21:45:37 +0300

    Preporation to layout screen

---
+++ ChatSwift/ChatSwift/Controllers/PeopleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleViewController.swift
+//  ChatSwift
+//
+//  Created by Павел Чернышев on 14.03.2021.
+//
+
+import UIKit
+
+class PeopleViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+    
+}
